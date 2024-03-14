29d02144dd0bbf064146c11a050d1eb52045d7b0
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-03-14 10:12:44 -0300

    Controller

---
+++ src/main/java/br/com/henriquerodrigues/main/PrimeiraController.java
@@ -0,0 +1,14 @@
+package br.com.henriquerodrigues.main;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  //Receber Requisição - processar e retornar para o user
+@RequestMapping("/primeiraController") //Mapear solicitações para métodos de controladores.
+public class PrimeiraController {
+    @GetMapping("/primeiroMetodo")
+    public String primeiroMetodo() {
+        return "Meu primeiro metodo api rest";
+    }
+}
