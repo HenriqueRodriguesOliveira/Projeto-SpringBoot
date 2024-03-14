f69191ffe3c9ac175d54340046dba3699890b148
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-03-14 10:20:39 -0300

    Component Scan

--- src/main/java/br/com/henriquerodrigues/main/PrimeiroProjetoSpringbootApplication.java
+++ src/main/java/br/com/henriquerodrigues/main/PrimeiroProjetoSpringbootApplication.java
@@ -2,8 +2,10 @@ package br.com.henriquerodrigues.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.henriquerodrigues")
 public class PrimeiroProjetoSpringbootApplication {
 
 	public static void main(String[] args) {
